da00a32e0b6fecbadeb8c3437355204114988aa1
Daniel Hallenberg <hallenberg81@gmail.com>
2021-02-16 11:31:00 +0100

    Add project files.

---
+++ Lab3Console/Chunk.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3Console
+{
+    public class Chunk
+    {
+        public int ChunkNr { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public string Data { get; set; }
+
+
+        public Chunk(int chunkNr, string name, int size, string data)
+        {
+            ChunkNr = chunkNr;
+            Name = name;
+            Size = size;
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            if (Size > 1500)
+            {
+               
+                return $"\n=====Chunk=====\nChunk #: {ChunkNr}\nChunk Type: {Name}\nSize: {(Size/1000)} kB, {Data}\n===============";
+            }
+            else
+            {
+                return $"\n=====Chunk=====\nChunk #: {ChunkNr}\nChunk Type: {Name}\nSize: {Size} bytes, {Data}\n===============";
+            }
+        }
+    }
+}
---
+++ Lab3Console/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Lab3Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test path C:\Users\glimm\source\repos\Lab3MetaData\test2
+            string path;
+            string pathFull;
+            byte[] fileInByte;
+            List<Chunk> chunks = new List<Chunk>();
+
+            Console.WriteLine("Enter file path with or without extension: ");
+            path = Console.ReadLine();
+
+            pathFull = GetFile(path);
+
+            fileInByte = FileToByte(pathFull);
+
+            FileTypeFromBin(fileInByte);
+
+            chunks = GetChunks(fileInByte);
+
+            for (int i = 0; i < chunks.Count; i++)
+            {
+                Console.WriteLine(chunks[i].ToString());
+            }
+            
+            static byte[] FileToByte(string pathFull)
+            {
+                byte[] fileInByte;
+
+                try
+                {
+                    fileInByte = File.ReadAllBytes(pathFull);
+
+                    return fileInByte;
+                }
+                catch (ArgumentException)
+                {
+                 
+                }
+
+                return null;
+            }
+
+            static string GetFile(string path) 
+            {
+                string filename;
+                string fullPath;
+
+                if (path.Substring(path.Length -3).Equals('.'))
+                {
+                    filename = path.Substring(path.LastIndexOf('\\') + 1, path.LastIndexOf('.'));
+                }
+                else
+                {
+                    filename = path.Substring(path.LastIndexOf('\\') + 1);
+                    filename += ".*";
+
+                    path = path.Substring(0, path.LastIndexOf('\\') + 1);
+                }
+
+                try
+                {
+                    fullPath = System.IO.Directory.GetFiles(path, filename).First();
+
+                    return fullPath;
+                }
+                catch(InvalidOperationException)
+                {
+                    Console.WriteLine("File not found!!!");
+                }
+                catch(ArgumentException)
+                {
+                    Console.WriteLine("Invalid path!!!!");
+                    
+                }
+
+                return string.Empty;
+            }
+
+            static void FileTypeFromBin(byte[] file)
+            {
+                int resWidth;
+                int resHight;
+
+                if (file != null)
+                {
+                    if (file[0] == 66 && file[1] == 77)
+                    {
+                        resWidth = BitConverter.ToInt32(file, 18);
+                        resHight = BitConverter.ToInt32(file, 22);
+
+                        Console.WriteLine($"\nThis is an bmp file whith the resolution of {resWidth}x{resHight}");
+                    }
+                    else if (file[1] == 80 && file[2] == 78 && file[3] == 71)
+                    {
+                        byte[] tempW = { file[16], file[17], file[18], file[19] };
+                        byte[] tempH = { file[20], file[21], file[22], file[23] };
+
+                        string hexW = BitConverter.ToString(tempW).Replace("-", string.Empty);
+                        string hexH = BitConverter.ToString(tempH).Replace("-", string.Empty);
+
+                        resWidth = Convert.ToInt32(hexW, 16);
+                        resHight = Convert.ToInt32(hexH, 16);
+
+                        Console.WriteLine($"\nThis is an PNG file whith the resolution of {resWidth}x{resHight}");                       
+                    }
+                    else
+                    {
+                        Console.WriteLine("This file is not valid");
+                    }
+                }
+
+            }
+
+            static List<Chunk> GetChunks(byte[] byteChar)
+            {
+                List<Chunk> chunks = new List<Chunk>();
+                string[] chunkers = { "IHDR", "PLTE", "IDAT", "IEND", "TRNS", "CHRM", "GAMA", "ICCP", "SBIT", "SRGB", "ITXT", "TEXT", "ZTXT", "BKGB", "PHYS", "SPLT", "TIME" };
+                string checkLabel = "", chunkDataString, tempHex;
+                int chunkNr = 0, size;
+                bool isChunk;
+
+
+                if (byteChar != null)
+                {
+                    for (int i = 0; i < byteChar.Length - 4; i++)
+                    {
+                        byte[] temp = { byteChar[i], byteChar[i + 1], byteChar[i + 2], byteChar[i + 3] };
+                        tempHex = BitConverter.ToString(temp).Replace("-", string.Empty);
+
+                        for (int j = 0; j < tempHex.Length; j += 2)
+                        {
+                            checkLabel += (char)Int16.Parse(tempHex.Substring(j, 2), NumberStyles.AllowHexSpecifier);
+                        }
+
+                        isChunk = chunkers.Contains(checkLabel, StringComparer.OrdinalIgnoreCase);
+
+                        if (isChunk)
+                        {
+                            chunkNr++;
+                            byte[] tempSize = { byteChar[i - 4], byteChar[i - 3], byteChar[i - 2], byteChar[i - 1] };
+                            tempHex = BitConverter.ToString(tempSize).Replace("-", string.Empty);
+                            chunkDataString = BitConverter.ToString(tempSize);
+                            size = Convert.ToInt32(tempHex, 16);
+                            chunks.Add(new Chunk(chunkNr, checkLabel, size, chunkDataString));
+                        }
+
+                        checkLabel = "";
+                    } 
+                }
+            
+                return chunks;
+            }
+
+            Console.WriteLine("Hit any key......");
+            Console.Read();
+            
+        }
+    }
+}
